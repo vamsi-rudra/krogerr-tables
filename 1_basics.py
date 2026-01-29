578d816e56cc0f3769a9c1f21270afd3ed16ce64
vamsi-rudra <158691076+vamsi-rudra@users.noreply.github.com>
2026-01-28 18:17:44 -0600

    Add files via upload

---
+++ 1_basics.py
@@ -0,0 +1,24 @@
+import threading
+from concurrent.futures import ThreadPoolExecutor
+
+def transformation(input):
+
+    src = input['src']
+    dest = input['dest']
+
+    print(f"Reading from {src}")
+    print(f"Transforming.....")
+    print(f"Writing data to {dest}")
+    return f"Done {src} to {dest}"
+
+array = [
+    {"src":"table-1","dest":"table-1"},
+    {"src":"table-2","dest":"table-2"},
+    {"src":"table-3","dest":"table-3"}
+]
+
+with ThreadPoolExecutor(max_workers=3) as executor: 
+
+    my_futures = executor.map(transformation,array)
+
+print(f"The returned values are {list(my_futures)}")
